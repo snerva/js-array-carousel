a116c46ee5655583e22c0c80f660ab201fd448ec
snerva <selenenerva@live.it>
2022-10-12 15:48:23 +0200

    initial commit mileston 1

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//creare un array con images
+const slideImg = [
+<img src="./assets/img/01.webp" alt="">,
+<img src="./assets/img/02.webp" alt="">,
+<img src="./assets/img/03.webp" alt="">,
+<img src="./assets/img/04.webp" alt="">,
+<img src="./assets/img/05.webp" alt="">,
+];
