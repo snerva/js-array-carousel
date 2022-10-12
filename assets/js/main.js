fdbebb70461063124e3751209124fc0449b4dee2
snerva <selenenerva@live.it>
2022-10-12 15:51:03 +0200

    array images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 //creare un array con images
-const slideImg = [
-<img src="./assets/img/01.webp" alt="">,
-<img src="./assets/img/02.webp" alt="">,
-<img src="./assets/img/03.webp" alt="">,
-<img src="./assets/img/04.webp" alt="">,
-<img src="./assets/img/05.webp" alt="">,
+const slidesImgs = [
+'img/01.webp',
+'img/02.webp',
+'img/03.webp',
+'img/04.webp',
+'img/05.webp'
 ];
